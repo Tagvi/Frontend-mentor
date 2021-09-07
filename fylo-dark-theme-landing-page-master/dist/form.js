43a0f4921359480aadd26b39c9ed59583edab8af
Otar Natsvaladze <Oxyyyy366@gmail.com>
2021-09-07 21:49:46 +0400

    changed regex pattern

--- fylo-dark-theme-landing-page-master/dist/form.js
+++ fylo-dark-theme-landing-page-master/dist/form.js
@@ -1,17 +1,16 @@
-const error = document.querySelector(".error")
-const form = document.querySelector(".form")
-const email = document.querySelector(".email")
-const emailpattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
-form.addEventListener('submit', (e) =>{
-    let errors = []
-    if(!(email.value.match(emailpattern))){
-        errors.push('Please enter a valid email')
-    }
-    if(errors.length > 0){
-        e.preventDefault()
-        error.classList.remove('hidden')
-        error.innerText = errors
-        email.style.border = "2px solid red"
-    }
-    
-})
+const error = document.querySelector('.error');
+const email = document.querySelector('.email');
+const emailpattern =
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})/gim;
+document.querySelector('.form').addEventListener('submit', (e) => {
+  let errors = [];
+  if (!email.value.match(emailpattern)) {
+    errors.push('Please enter a valid email');
+  }
+  if (errors.length > 0) {
+    e.preventDefault();
+    error.classList.remove('hidden');
+    error.innerText = errors;
+    email.style.border = '2px solid red';
+  }
+});
